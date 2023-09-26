f789d2cf4ac474cac7bbd046154fdba268a25c28
Mohamed Mahmoud <mohammedarey2@gmail.com>
2023-09-26 17:56:44 +0300

    Cleaning up

--- main.go
+++ main.go
@@ -6,25 +6,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
 func main() {
 	router := gin.Default()
-	router.GET("/albums", scrape)
+	router.GET("/scrape", scrape)
 
 	router.Run("localhost:8000")
 }
 
 func scrape(ctx *gin.Context) {
 	url := ctx.Request.URL.Query().Get("url")
+
 	if url == "" {
 		ctx.IndentedJSON(http.StatusUnprocessableEntity, gin.H{"message": "Please enter a url"})
 		return
 	}
+
 	ctx.IndentedJSON(http.StatusOK, gin.H{"data": url})
 }
